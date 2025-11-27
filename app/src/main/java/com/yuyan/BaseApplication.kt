37c1025f4ed02b49d41f7d1bf3ba4a929589fb80
gure <gurecn@163.com>
2025-11-27 21:53:23 +0800

    fix(code):增加崩溃日志保存功能

--- app/src/main/java/com/yuyan/BaseApplication.kt
+++ app/src/main/java/com/yuyan/BaseApplication.kt
@@ -2,6 +2,7 @@ package com.yuyan
 
 import android.app.Application
 import com.yuyan.imemodule.application.Launcher
+import com.yuyan.imemodule.utils.CrashHandler
 
 /**
  * Applicaiton入口
@@ -10,6 +11,7 @@ import com.yuyan.imemodule.application.Launcher
 class BaseApplication : Application(){
     override fun onCreate() {
         super.onCreate()
+        CrashHandler.instance.init(this)
         Launcher.instance.initData(baseContext)
     }
 }
